2c347bee30c8338dd0577f23d35a49784667460c
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2022-08-03 19:32:49 -0600

    Update compile.ts

--- compile.ts
+++ compile.ts
@@ -1,10 +1,26 @@
-// compile code will go here
-import path from 'path';
-import fs from 'fs';
-import solc from 'solc';
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
 
-// c:/Users/user/Desktop/contracts/Inbox.js
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*'],
+      },
+    },
+  },
+};
+
+module.exports = JSON.parse(
+  solc.compile(JSON.stringify(input))
+).contracts['Inbox.sol'].Inbox;
